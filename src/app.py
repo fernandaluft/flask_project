44999a27e876dd963272e608c2f21301db016203
fernandaluft <108574888+fernandaluft@users.noreply.github.com>
2024-01-16 21:54:05 +0000

    update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,38 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, render_template, request, redirect, url_for
+from pickle import load
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
-# your code here
+app = Flask(__name__)
+
+vector = load(open("../models/vector_tfidf.sav", "rb"))
+model = load(open("../models/svm_sentiment_analysis.sav", "rb"))
+
+def predict_sentiment(str):
+    sentence = [str.strip().lower().replace('\t', ' ').replace('\n', ' ').replace('.', '')]
+    sentence_vector = vector.transform(sentence).toarray()
+    prediction = model.predict(sentence_vector)
+    if prediction == 1:
+        return 'Positive'
+    else:
+        return 'Negative'
+
+@app.route('/', methods=['GET', 'POST'])
+def rootpage():
+    review = ''
+    result = ''
+
+    if request.method == 'POST' and 'review' in request.form:
+        review = request.form.get('review')
+        if review is None:
+            result = 'Enter a review'
+        else:
+            result = predict_sentiment(review)
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run()
